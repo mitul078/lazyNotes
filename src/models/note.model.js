5cf6a11af1d52a50b1b1b5683475237dd8018000
mitul078 <mituljodhani788@gmail.com>
2025-07-11 20:08:44 +0530

    FIX

--- src/models/note.model.js
+++ src/models/note.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema({
     title:String,
-    content: String
+    content: String,
 })
 
 const noteModel = mongoose.model("note" , noteSchema)
